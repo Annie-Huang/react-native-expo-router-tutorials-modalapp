89caf7b3b7d15b2c141212e754874ad2aa925c90
Annie Huang <annie.hn@gmail.com>
2025-12-25 16:10:33 +1100

    Create the app/modal.tsx

---
+++ app/modal.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Text, View, StyleSheet } from 'react-native';
+
+const Modal = () => {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Modal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
